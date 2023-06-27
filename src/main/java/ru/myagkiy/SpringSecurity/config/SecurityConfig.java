a7c62a43c8398faaa8080b89f35fcc6382136caa
sergey.myagkiy <sergey.myagkiy@rt.ru>
2023-06-27 12:20:19 +0300

    Убран authProviderImpl, добавлена кастомная форма авторизации

--- src/main/java/ru/myagkiy/SpringSecurity/config/SecurityConfig.java
+++ src/main/java/ru/myagkiy/SpringSecurity/config/SecurityConfig.java
@@ -7,20 +7,55 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import ru.myagkiy.SpringSecurity.security.AuthProviderImpl;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import ru.myagkiy.SpringSecurity.services.PersonDetailService;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-    private final AuthProviderImpl authProvider;
+    private final PersonDetailService personDetailService;
 
+    //Аутентификация пользователя
     @Bean
     public AuthenticationManager authManager(HttpSecurity http) throws Exception {
         AuthenticationManagerBuilder authenticationManagerBuilder =
                 http.getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder.authenticationProvider(authProvider);
+        authenticationManagerBuilder.userDetailsService(personDetailService);
         return authenticationManagerBuilder.build();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                //отключаем защиту межсайтовой подделки запросов
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        //Разрешаем не авторизованному пользователю доступ на страницу login
+                        auth -> auth.requestMatchers("/auth/login")
+                                .permitAll()
+                                // для всех остальных страниц нужна аутентификация
+                                .anyRequest()
+                                .authenticated())
+                //указываем адрес кастомной страницы аутентификации
+                .formLogin(form -> form
+                        .loginPage("/auth/login")
+                        //указываем адрес страницы на которую будет отправлена форма с login.html
+                        .loginProcessingUrl("/process_login")
+                        //если пользовательские данные были введены верно, то перенаправляем на страницу
+                        .defaultSuccessUrl("/hello", true)
+                        //если пользовательские данные были введены неверно, то перенаправляем на страницу
+                        .failureUrl("/auth/login?error"))
+                .build();
+    }
+
+    //Указываем способ шифрования пароля пользователя(сейчас нет шифрования)
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
 
---
+++ src/main/java/ru/myagkiy/SpringSecurity/controller/LoginController.java
@@ -0,0 +1,14 @@
+package ru.myagkiy.SpringSecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")
+public class LoginController {
+    @GetMapping("/login")
+    public String login() {
+        return "auth/login";
+    }
+}
